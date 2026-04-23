bf3d85b82c50ddcba0c0dab896c7b9e0fe72fda8
dhanith-bandara <dhanith.20233048@iit.ac.lk>
2026-04-24 01:22:50 +0530

    Add Sensor model
    Introduce Sensor POJO (com.smartcampus.models) to represent a sensor entity with id, type, status, currentValue and roomId. Includes default and parameterized constructors, getters/setters, a List<Link> _links for HATEOAS-style links and an addLink(rel, href) helper.

---
+++ src/main/java/com/smartcampus/models/Sensor.java
@@ -0,0 +1,75 @@
+package com.smartcampus.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sensor {
+    private String id;
+    private String type;
+    private String status;
+    private double currentValue;
+    private String roomId;
+    private List<Link> _links = new ArrayList<>();
+
+    public Sensor() {}
+
+    public Sensor(String id, String type, String status, double currentValue, String roomId) {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.currentValue = currentValue;
+        this.roomId = roomId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public List<Link> getLinks() {
+        return _links;
+    }
+
+    public void setLinks(List<Link> _links) {
+        this._links = _links;
+    }
+
+    public void addLink(String rel, String href) {
+        this._links.add(new Link(rel, href));
+    }
+}
